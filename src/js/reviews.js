b4060042a3eb90c82f16909ce0e5fd2d6293a4ca
Oleksii Donets <108811517+Davzp1980@users.noreply.github.com>
2024-07-05 09:55:09 +0300

    markup, css, js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,1 +1,65 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import axios from 'axios';
 
+const swiperOptions = {
+  //   loop: true,
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // autoHeight: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1439: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+  },
+};
+const swiper = new Swiper('.swiper', swiperOptions);
+
+const slideWrapper = document.querySelector('.reviews-swiper-wrapper');
+
+async function getReviews() {
+  const res = await axios.get('https://portfolio-js.b.goit.study/api/reviews', {
+    headers: {
+      accept: 'application/json',
+    },
+  });
+  console.log(res.data);
+  slideWrapper.insertAdjacentHTML('afterbegin', slidesTemplate(res.data));
+}
+
+function slideTemplate(slide) {
+  return `<div class="swiper-slide reviews-swiper-slide">
+          <img
+            class="reviews-img"
+            src="${slide.avatar_url}"
+            alt="Natalia"
+          />
+          <div class="reviews-desc">
+            <p class="reviews-name">${slide.author}</p>
+            <p class="reviews-text">${slide.review}</p>
+          </div>
+        </div>`;
+}
+
+function slidesTemplate(slades) {
+  return slades.map(slideTemplate).join('');
+}
+
+getReviews();
