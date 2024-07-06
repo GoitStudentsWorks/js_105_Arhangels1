33f758c76ac078fa55c024b5a5b580504492cf43
Ilya Svintsov <xeri237@gmail.com>
2024-07-06 15:02:17 +0300

    do accordion list without swiper part

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,1 +1,33 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
+new Accordion('.about-me__list');
+
+
+// const headers = document.querySelectorAll('.about-me__list__item__header');
+// const contents = document.querySelectorAll('.about-me__list__item__content');
+//
+// headers.forEach(header => header.addEventListener('click', onHeaderClick));
+//
+// function onHeaderClick(e) {
+//   const activeContent = document.getElementById(e.target.dataset.tab);
+//
+//   if (activeContent.classList.contains('active')) {
+//     e.target.classList.remove('active');
+//     activeContent.classList.remove('active');
+//     activeContent.style.maxHeight = 0;
+//   } else {
+//     contents.forEach(content => {
+//       content.classList.remove('active');
+//       content.style.maxHeight = 0;
+//     });
+//
+//     headers.forEach(header => header.classList.remove('active'));
+//
+//     e.target.classList.add('active');
+//     activeContent.classList.add('active');
+//
+//     // Устанавливаем maxHeight элемента для плавного открытия
+//     activeContent.style.maxHeight = `${activeContent.scrollHeight}px`;
+//   }
+// }
