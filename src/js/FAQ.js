85f14db6b9c647761cbfb5e469faf4bec9c892f7
Kateryna Yershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-07-06 23:01:42 +0300

    add faq section

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const faqItems = document.querySelectorAll('.faq-item');
 
+  faqItems.forEach(item => {
+    item.querySelector('.faq-question').addEventListener('click', () => {
+      item.classList.toggle('active');
+
+      const answer = item.querySelector('.faq-answer');
+      if (item.classList.contains('active')) {
+        answer.style.display = 'block';
+      } else {
+        answer.style.display = 'none';
+      }
+    });
+  });
+});
