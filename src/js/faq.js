90960280d9239ced718c31ecea2c62fa0630c645
Kateryna Yershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-07-07 21:53:12 +0300

    add changes to js

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,19 +3,30 @@ import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.faq-all-questions');
 
-document.addEventListener('DOMContentLoaded', function () {
-  const faqItems = document.querySelectorAll('.faq-item');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const accordion = new Accordion('.faq-all-questions', {
+//     duration: 300,
+//     showMultiple: false,
+//     openOnInit: [],
+//   });
 
-  faqItems.forEach(item => {
-    item.querySelector('.faq-question').addEventListener('click', () => {
-      item.classList.toggle('active');
+//   const faqItems = document.querySelectorAll('.faq-item');
 
-      const answer = item.querySelector('.faq-answer');
-      if (item.classList.contains('active')) {
-        answer.style.display = 'block';
-      } else {
-        answer.style.display = 'none';
-      }
-    });
-  });
-});
+//   faqItems.forEach(item => {
+//     const question = item.querySelector('.faq-question');
+//     const answer = item.querySelector('.faq-answer');
+
+//     question.addEventListener('click', () => {
+//       const isActive = item.classList.contains('active');
+//       faqItems.forEach(i => {
+//         i.classList.remove('active');
+//         i.querySelector('.faq-answer').style.display = 'none';
+//       });
+
+//       if (!isActive) {
+//         item.classList.add('active');
+//         answer.style.display = 'block';
+//       }
+//     });
+//   });
+// });
