3bb66a4c454eb08d1bff38016445bffe21671414
Kateryna Yershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-07-07 21:57:11 +0300

    add changes to js

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,31 +2,3 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.faq-all-questions');
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const accordion = new Accordion('.faq-all-questions', {
-//     duration: 300,
-//     showMultiple: false,
-//     openOnInit: [],
-//   });
-
-//   const faqItems = document.querySelectorAll('.faq-item');
-
-//   faqItems.forEach(item => {
-//     const question = item.querySelector('.faq-question');
-//     const answer = item.querySelector('.faq-answer');
-
-//     question.addEventListener('click', () => {
-//       const isActive = item.classList.contains('active');
-//       faqItems.forEach(i => {
-//         i.classList.remove('active');
-//         i.querySelector('.faq-answer').style.display = 'none';
-//       });
-
-//       if (!isActive) {
-//         item.classList.add('active');
-//         answer.style.display = 'block';
-//       }
-//     });
-//   });
-// });
